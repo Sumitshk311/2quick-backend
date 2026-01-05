920bbb5d643a08f64dbdb97681a60122384c76ec
Sumit Soni <sumit311shk@gmail.com>
2026-01-05 13:24:25 +0530

    Something Backend update

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const connectDB = async () => {
 
 // Start server
 const startServer = () => {
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 10000;
   app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
 };
 
